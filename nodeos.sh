5d00f6fc60e9c034eaccc58e1bf479a2a157e681
Thiago Souza <tcostasouza@gmail.com>
2021-12-03 17:49:59 +0000

    fix plugins

--- nodeos.sh
+++ nodeos.sh
@@ -12,8 +12,7 @@ nodeos \
     --plugin eosio::chain_plugin \
     --plugin eosio::chain_api_plugin \
     --plugin eosio::http_plugin \
-    --plugin eosio::history_api_plugin \
-    --plugin eosio::history_plugin \
+    --plugin eosio::state_history_plugin \
     --producer-name eosio \
     --enable-stale-production \
     --access-control-allow-origin='*' \
